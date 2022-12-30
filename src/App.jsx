8d55b138b58f6dccdaf637567f338b46b896fa57
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2022-12-30 13:54:00 +0600

    create boilerplate template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="bg-orange-800">
+     <h1 className="flex items-center justify-center min-h-screen font-bold text-7xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
