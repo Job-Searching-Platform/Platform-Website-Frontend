e351795d34210797536c48eb308d2c85611710b5
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2023-01-03 10:55:48 +0600

    update sign in

--- src/pages/Authentication/SignIn.jsx
+++ src/pages/Authentication/SignIn.jsx
@@ -4,35 +4,47 @@ const SignIn = () => {
     <section class="relative py-0 bg-white lg:py-20">
         <div class="flex flex-col items-center justify-between px-10 mx-auto max-w-7xl xl:px-5 lg:flex-row">
 
-            <div class="flex flex-col items-center w-full px-10 pt-5 pb-20 lg:pt-20 lg:flex-row">
-                <div class="relative w-full max-w-md bg-cover lg:max-w-2xl lg:w-7/12">
+            <div class="flex flex-col items-center w-full px-10 pt-5 pb-20 ">
+                {/* <div class="relative w-full max-w-md bg-cover lg:max-w-2xl lg:w-7/12">
                     <div class="relative flex flex-col items-center justify-center w-full h-full lg:pr-10">
                         <img src="https://cdn.devdojo.com/images/december2020/taxi-programming.png" alt=''/>
                     </div>
-                </div>
+                </div> */}
 
                 <div class="relative z-10 w-full max-w-2xl mt-20 lg:mt-0 lg:w-5/12">
                     <div class="relative z-10 flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl">
-                        <h4 class="w-full font-serif text-4xl font-medium leading-snug">Schedule a Demo <br />of our product</h4>
+                        <h4 class="w-full items-center justify-center font-serif text-4xl font-medium leading-snug">Sign In</h4>
                         <div class="relative w-full mt-6 space-y-8">
-                            <div class="relative">
-                                <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">First Name</label>
-                                <input type="text" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="John" />
+                            <div class="px-3 text-center">
+                                <a class=" mb-4 p-4 flex justify-center items-center border rounded hover:bg-gray-50" href="/">
+                                <img class="mr-4" src="https://shuffle.dev/atis-assets/social/facebook-logo.png" alt="" />
+                                <span class="text-xs text-gray-500 font-bold transition duration-200" data-config-id="fb-action">Sign In with Facebook</span>
+                                </a>
+                                <a class="p-4 flex justify-center items-center border rounded hover:bg-gray-50" href="/">
+                                <img class="mr-4" src="https://shuffle.dev/atis-assets/social/google-logo.png" alt="" />
+                                <span class="text-xs text-gray-500 font-bold transition duration-200" data-config-id="google-action">Sign In with Google</span>
+                                </a>
                             </div>
-                            <div class="relative">
-                                <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Last Name</label>
-                                <input type="text" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="Doe" />
-                            </div>
+                            <div className="border-1 border-white flex items-center justify-center"><h4>or</h4></div>
                             <div class="relative">
                                 <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Email Address</label>
                                 <input type="text" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="janedoe@email.com" />
                             </div>
                             <div class="relative">
-                                <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Phone</label>
-                                <input type="number" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="Phone Number" />
+                                <label class="absolute px-2 ml-2 -mt-3 font-medium text-gray-600 bg-white">Password</label>
+                                <input type="password" class="block w-full px-4 py-4 mt-2 text-base placeholder-gray-400 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-black" placeholder="Password" />
                             </div>
+                            <label class="inline-flex mb-10 text-left">
+                                <input class="mr-2" type="checkbox" name="terms" value="1" />
+                                <span class="-mt-1 inline-block text-sm text-gray-500" data-config-id="terms">By signing up, you agree to our <a class="text-red-400 hover:underline" href="/">Terms, Data Policy</a> and <a class="text-red-400 hover:underline" href="/">Cookies Policy</a>.</span>
+                            </label>
                             <div class="relative">
                                 <a href="#_" class="inline-block w-full px-5 py-4 text-xl font-medium text-center text-white transition duration-200 bg-yellow-300 rounded-lg hover:bg-yellow-400 ease">Submit</a>
+                            </div>
+
+                            <div className="flex items-center justify-center">
+                            <a class="text-xs text-green-600 hover:underline" href="/" data-config-id="03_secondary-action">Forgot password?</a>
+
                             </div>
 
                         </div>
@@ -44,6 +56,7 @@ const SignIn = () => {
             </div>
 
         </div>
+       
    </section>
   )
 }
