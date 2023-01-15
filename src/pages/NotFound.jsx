546dbe1f74de0d1773af87dc84a113c76a10cba0
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2023-01-15 13:29:18 +0600

    create not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,34 @@
-
+import Error404 from "../assets/Error404";
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen text-7xl"><h1>Not Found</h1></div>
-  )
-}
+    <div className="flex justify-center items-center">
+      <div className="w-1/6 pl-10">
+        <a
+          href="/"
+          className="border-1 border-gray-700 w-6 h-6 text-red-500"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 "
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+        </a>
+      </div>
+      <div className="w-5/6">
+        <Error404 />
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
